e60b5b127d0fcbe30ab0d76e1f54be3727c51476
Antony <34607019+AntonyBazin@users.noreply.github.com>
2022-10-17 16:48:01 +0300

    Updated task8 from fopen to system calls

--- lab1/task8.c
+++ lab1/task8.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
-void copy_file(FILE*, FILE*);
-
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    int fd1, fd2;
     char* file1, *file2;
     int got_input = 0;
+    char buffer[1024] = {0};
+    long int bytes_count = 1024;
+
     if (argc == 3) {
         file1 = argv[1];
         file2 = argv[2];
@@ -22,21 +27,27 @@ int main(int argc, char *argv[]){
         printf("Usage: %s [file1 file2]\n", argv[0]);
         exit(1);
     }
-    FILE* from = fopen(file1, "r");
-    FILE* to = fopen(file2, "w");
-    copy_file(to, from);
-    fclose(to);
-    fclose(from);
+
+    if(((fd1 = open(file1, O_RDONLY)) == -1) || ((fd2=open(file2, O_CREAT|O_WRONLY|O_TRUNC, 0755)) == -1)){
+        perror("Problem reading input");
+        exit(1);
+    }
+    //if(got_input)
+    //fchmod(fd2, 0755)
+
+     while((bytes_count = read(fd1, buffer, 1024)) > 0){
+        printf("Read %ld bytes\n", bytes_count);
+        if(write(fd2, buffer, bytes_count) != bytes_count){
+            perror("Writing error");
+            exit(1);
+        }
+    }
+    close(fd1);
+    close(fd2);
     if (got_input) {
         free(file1);
         free(file2);
     }
-    return 0;
+    printf("Successfully wrote all the contents!\n");
+    exit(0);
 }
-
-void copy_file(FILE* to, FILE* from){
-    int rc;
-    while ((rc = fgetc(from)) != EOF)
-        fputc(rc, to);
-}
-
