2dbf80566d00d38c219e567ce6c8d95848545a16
AntonyBazin <antonyvbazin@gmail.com>
2022-10-19 19:42:41 +0300

    Added tasks 1 and 2

---
+++ lab2/task1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* read_env(char*, char*);
+
+int main(){
+	char name[] = "my_name";
+	char value[] = "my_value";
+	char new_value[] = "new_value";
+	char* ptr;
+	if(setenv(name, value, 1)){
+		perror("Error setting env variable");
+	}
+	read_env(name, ptr);
+	if(setenv(name, new_value, 1)){
+		perror("Error changing env variable");
+	}
+	read_env(name, ptr);
+	if(setenv(name, value, 0)){
+		perror("Error when not changing env variable");
+	}
+	read_env(name, ptr);
+	exit(0);
+}
+
+
+char* read_env(char* name, char *ptr) {
+	if (!(ptr = getenv(name))) {
+		perror("Error reading env variable");
+		exit(1);
+	}
+	printf("%s\n", ptr);
+	return ptr;
+}
+// Обзательно дочерний процесс делает setpgrp()
+//
---
+++ lab2/task2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+	printf("I am father before fork. My pid is %d, my ppid is %d\n", getpid(), getppid());
+	if (fork()){
+		printf("I am father after fork. My pid is %d, my ppid is %d\n", getpid(), getppid());
+		wait(NULL);
+		exit(0);
+	}
+	else{
+		printf("I am son. My pid is %d, my ppid is %d\n", getpid(), getppid());
+		exit(0);
+	}
+}
