f0293a0f4f6df56bb4fb477964076885546c3b39
AntonyBazin <antonyvbazin@gmail.com>
2022-10-30 11:11:44 +0300

    Tasks 5-8 done

--- lab3/task5.c
+++ lab3/task5.c
@@ -2,15 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 
+extern int errno;
+/*
+If the child process for which status was returned 
+by the wait or waitpid function exited because it 
+raised a signal that caused it to exit, the WIFSIGNALED
+macro evaluates to TRUE and the WTERMSIG macro can be
+used to determine which signal was raised by the child process.
+Otherwise, the WIFSIGNALED macro evaluates to FALSE.
+*/
 
 int main(void){
 	int pid;
 	int return_code;
+	int wait_code;
 	if((pid = fork())){
-		wait(&return_code);
+		sleep(1);
+		printf("Sending to child signal %d\n", SIGUSR1);
+		kill(pid, SIGUSR1);
+		wait_code = wait(&return_code);
+		if (WIFSIGNALED(return_code))
+			printf("Child was stopped by %d\n", WTERMSIG(return_code));
+
 		printf("Child exited with %d\n", return_code);
+		printf("Got %d from wait\n", wait_code);
+		printf("Set errno: %d\n", errno);
 	} else {
 		printf("I am son. My pid is %d, my ppid is %d\n", getpid(), getppid());
 		for (int i = 0; i < 100; ++i) {
--- lab3/task7.c
+++ lab3/task7.c
@@ -1,4 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+
+extern int errno;
+
+// Answer: to use signals and signal handlers.
+/*
+pause() causes the calling process (or thread) to sleep until a
+signal is delivered that either terminates the process or causes
+the invocation of a signal-catching function.
+*/
+void sighandler(int);
+
+
 int main(void){
-	
+	int pid;
+	int return_code;
+	int wait_code;
+	signal(SIGUSR1, sighandler);
+	if((pid = fork())){
+		sleep(1);
+		printf("Sending to child signal %d\n", SIGUSR1);
+		kill(pid, SIGUSR1);
+		wait_code = wait(&return_code);
+		if (WIFSIGNALED(return_code))
+			printf("Child was stopped by %d\n", WTERMSIG(return_code));
+
+		printf("Chiled exited with %d\n", return_code);
+		printf("Got %d from wait\n", wait_code);
+		printf("Set errno: %d\n", errno);
+	} else {
+		printf("I am son. My pid is %d, my ppid is %d\n", getpid(), getppid());
+		pause();
+	}
 	return 0;
+}
+
+void sighandler(int signum){
+	printf("This is my custom handler.\n");
+	signal(signum, SIG_DFL);
 }
